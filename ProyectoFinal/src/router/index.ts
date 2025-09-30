6e667375f64a4da0534f1059d306bffcaec96cec
j4ik0-dev <salazzarjefferson@gmail.com>
2025-09-29 21:50:55 -0600

    cambio en index.ts

--- ProyectoFinal/src/router/index.ts
+++ ProyectoFinal/src/router/index.ts
@@ -1,60 +1,41 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import Inicio from '../views/Inicio.vue'
-import Pagina1 from '../views/Pagina1.vue'
-import Perfil from '../views/Perfil.vue'
-import Pagina2 from '../views/Pagina2.vue'
-import Pagina3 from '../views/Pagina3.vue'
-
-interface MyRouteMeta {
-  title: string
-}
-
-const routes: Array<RouteRecordRaw & { meta?: MyRouteMeta }> = [
-  {
-    path: '/',
-    redirect: '/inicio'
-  },
-  {
-    path: '/inicio',
-    name: 'Inicio',
-    component: Inicio,
-    meta: { title: 'Inicio' }
-  },
-  {
-    path: '/pagina1',
-    name: 'Pagina1',
-    component: Pagina1,
-    meta: { title: 'Página 1' }
-  },
-  {
-    path: '/perfil',
-    name: 'Perfil',
-    component: Perfil,
-    meta: { title: 'Perfil' }
-  },
-  {
-    path: '/pagina2',
-    name: 'Pagina2',
-    component: Pagina2,
-    meta: { title: 'Página 2' }
-  },
-  {
-    path: '/pagina3',
-    name: 'Pagina3',
-    component: Pagina3,
-    meta: { title: 'Página 3' }
-  }
-]
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
-
-router.beforeEach((to, from, next) => {
-  document.title = (to.meta?.title as string) || 'ProyectoFinal'
-  next()
-})
-
+import { createRouter, createWebHistory } from '@ionic/vue-router'; 
+import { RouteRecordRaw } from 'vue-router'; 
+import Inicio from '../views/Inicio.vue' 
+import Pagina1 from '../views/Pagina1.vue' 
+import Perfil from '../views/Perfil.vue' 
+import Pagina2 from '../views/Pagina2.vue' 
+import Pagina3 from '../views/Pagina3.vue' 
+const routes: Array<RouteRecordRaw> = [
+    { 
+        path: '/', 
+        redirect: '/inicio' 
+    }, 
+    { 
+        path: '/inicio', 
+        name: 'Inicio', 
+        component: Inicio 
+    }, 
+    { 
+        path: '/pagina1', 
+        name: 'Pagina1', 
+        component: Pagina1 
+    }, 
+    { 
+        path: '/perfil', 
+        name: 'Perfil', 
+        component: Perfil 
+    }, 
+    { 
+        path: '/pagina2', 
+        name: 'Pagina2', 
+        component: Pagina2 
+    }, 
+    { 
+        path: '/pagina3', 
+        name: 'Pagina3', 
+        component: Pagina3 
+    }] 
+const router = createRouter({ 
+    history: createWebHistory(import.meta.env.BASE_URL), routes 
+}) 
 export default router
